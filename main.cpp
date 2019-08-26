542de8d06be7a8e91255e94fbb5d8095abf39840
Xingyu Wang <brandon.xyw@gmail.com>
2019-08-26 14:07:07 -0400

    fix teapot disappear problem in Windows

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <OpenGL/OpenGL.h>
 #include <GLUT/glut.h>
 
+// #include "GL/freeglut.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -43,28 +45,25 @@ void resize(int w, int h) {
 
 void display() 
 {
-    //printf("display\n");
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();  // reset the current matrix to identity matrix
 
     // create a viewing matrix defined by an eye point and a reference point
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
+    glViewport(0, 0, 480, 480);  // define a viewport and display in this viewport
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60.0, 1, 1.0, 20.0);  // specify a viewing frustum
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);  // def
+
     glRotatef(angle, 0.0f, 1.0f, 0.0f);
-
     glutSolidTeapot(0.5);
-
-    if (isRotating) angle += 1.0f;
-
     glutSwapBuffers();
 
-    // glViewport(0, 0, 480, 480);  // define a viewport and display in this viewport
-	// glMatrixMode(GL_PROJECTION);
-	// glLoadIdentity();
-	// gluPerspective(60.0, 1, 1.0, 20.0);  // specify a viewing frustum
-	// glMatrixMode(GL_MODELVIEW);
-	// glLoadIdentity();
-	// gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);  // def
+    if (isRotating) angle += 1.0f;
 }
 
 void mouseFunc(int button, int state, int x, int y)
@@ -102,7 +101,7 @@ int main(int argc, char *argv[])
     // register callback func
     glutDisplayFunc(display);
     glutKeyboardFunc(processNormalKey);
-    glutIdleFunc(idle);
+    glutIdleFunc(display);
     glutMouseFunc(mouseFunc);
     //glutReshapeFunc(resize);
 
